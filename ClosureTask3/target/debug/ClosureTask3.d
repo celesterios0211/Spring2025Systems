f9d467841bbc694e1d9167680fc5107952f1378c
celesterios0211 <celesterios0211@gmail.com>
2025-04-15 02:44:25 +0000

    Closure Task 3

---
+++ ClosureTask3/target/debug/ClosureTask3.d
@@ -0,0 +1,1 @@
+/workspaces/Spring2025Systems/ClosureTask3/target/debug/ClosureTask3: /workspaces/Spring2025Systems/ClosureTask3/src/main.rs
---
+++ ClosureTask3/target/debug/deps/ClosureTask3-39a0893b2ac76b60.d
@@ -0,0 +1,5 @@
+/workspaces/Spring2025Systems/ClosureTask3/target/debug/deps/ClosureTask3-39a0893b2ac76b60: src/main.rs
+
+/workspaces/Spring2025Systems/ClosureTask3/target/debug/deps/ClosureTask3-39a0893b2ac76b60.d: src/main.rs
+
+src/main.rs:
